54aea5a21b8ec364040dc3c69b0128fdbb5c4358
Dmitry Marov <mds3d14@gmail.com>
2020-03-21 10:56:18 +0300

    updated windows installer

--- install-windows.ps1
+++ install-windows.ps1
@@ -1,27 +1,27 @@
 choco install git -y
-choco install python3 -y
+choco install python -y
 #choco install python2 -y
 choco install yarn -y
 choco install nodejs -y
 choco install neovim -y
 choco install pip -y --allow-empty-checksums
-choco install curl -y
 
 #pip2.exe install neovim
-pip3.exe install neovim
+pip3 install neovim
+pip3 install --user pynvim
 yarn global add neovim
 
 # create folders
-mkdir "~/AppData/Local/nvim/autoload"
-mkdir "~/AppData/Local/nvim/plugged"
-mkdir "~/AppData/Local/nvim/colors"
+New-Item -ItemType Directory -Path "~/AppData/Local/nvim/autoload" -Force
+New-Item -ItemType Directory -Path "~/AppData/Local/nvim/plugged" -Force
+New-Item -ItemType Directory -Path "~/AppData/Local/nvim/colors" -Force
 
 # install plug
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -OutFile "$home/AppData/Local/nvim/autoload/plug.vim"
 
 # install config
-New-Item -ItemType SymbolicLink -Path "$home/AppData/Local/nvim/init.vim" -Target "$pwd/init.vim"
-New-Item -ItemType SymbolicLink -Path "$home/AppData/Local/nvim/ginit.vim" -Target "$pwd/ginit.vim"
+New-Item -ItemType SymbolicLink -Path "$home/AppData/Local/nvim/init.vim" -Target "$pwd/init-win.vim" -Force
+New-Item -ItemType SymbolicLink -Path "$home/AppData/Local/nvim/ginit.vim" -Target "$pwd/ginit.vim" -Force
 
 cd ~
 
